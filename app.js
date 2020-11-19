731ceb1c0fccb1f736904c487df24e7804485d75
john-david-cutler <john.david.cutler@gmail.com>
2020-11-18 18:58:59 -0700

    JQUERY_W5D4 assignment

---
+++ app.js
@@ -0,0 +1,5 @@
+$('.navItem').css('color','white');
+$('#nav').css('background-color','#1457');
+$('li:even').css('background-color','#ccc9');
+$('li:odd').css('background-color','#aaaaaa');
+$('input').val('John Cutler');
